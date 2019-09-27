fcf1c9e9c7d84ef435980c0380df05a75e665f72
Pavel Petrov <Petrov.P@postuf.com>
2019-09-27 18:58:22 +0300

    upd

--- bot.php
+++ bot.php
@@ -20,13 +20,41 @@ $config = Yaml::parseFile('./parameters.yml');
 // $client contains all the methods to interact with the API
 $client = JoliCode\Slack\ClientFactory::create($config['token']);
 
+// Получить пользователя slack по email:
+// https://api.slack.com/methods/users.lookupByEmail
+//
+// Цель: получить ID пользователя для отправки персональных сообщений и др. действий.
+$userByEmail = $client->usersLookupByEmail(
+    [
+        'email' => $config['user_email'],
+    ]
+);
 
+$userId = null;
+if ($userByEmail->getOk()) {
+    $userId = $userByEmail->getUser()->getId();
+} else {
+    throw new Exception('Cannot get user ID by email.');
+}
 
-//$user = $client->usersInfo(['user' => $config['user_id']])->getUser();
-//dump($user);
+// Отправить личное сообщение пользователю (используется его ID в workspace)
+// https://api.slack.com/methods/chat.postMessage
+$client->chatPostMessage(
+    [
+        'as_user' => true,
+        'channel' => $userId,
+        'text'    => 'test12345',
+    ]
+);
 
-$client->chatPostMessage([
-    'as_user' => true,
-    'channel' => $config['user_id'],
-    'text' => 'test123',
-]);
+// Создать приватный канал с пользователем для нотификаций.
+//
+// conversations.create - public/private channel creation.
+// conversations.open - direct message for 1 person or N persons.
+$resp = $client->groupsCreate(
+    [
+        'name' => 'test-private-channel-for-notifications',
+    ]
+);
+
+dump($resp);
